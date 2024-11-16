fc8d626bdbd13abfab59b467b8c495c8fecce914
shsim <sh227sim@gmail.com>
2024-11-16 15:00:48 +0900

    FE-FEAT: add login, signup, mypage link in navbar

--- movie-pjt/src/App.vue
+++ movie-pjt/src/App.vue
@@ -7,10 +7,16 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item mx-2">
+          <RouterLink :to="{name:'login'}">Log in</RouterLink>
+        </li>
+        <li class="nav-item mx-2">
+          <RouterLink :to="{name:'signup'}">Sign Up</RouterLink>
+        </li>
+        <li class="nav-item mx-2">
           <RouterLink :to="{name:'movies'}">영화 추천 목록</RouterLink>
         </li>
         <li class="nav-item mx-2">
-          <a class="nav-link" href="#">Link</a>
+          <RouterLink :to="{name:'profile'}">My Page</RouterLink>
         </li>
       </ul>
     </div>
