52e363027c813111891ed5fa1906d6e5ab4b9b4a
shsim <sh227sim@gmail.com>
2024-11-18 14:17:19 +0900

    FE-FEAT: update MovieListView and add component, movie card

---
+++ movie-pjt/src/components/MovieCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img :src="getImgUrl(movie.poster_path)" alt="">
+    <h5>{{ movie.title }}</h5>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  movie:Object
+})
+
+
+import { useMovieStore } from "@/stores/movie"
+
+const store = useMovieStore()
+
+const getImgUrl = function(poster_path) {
+  return `${store.IMAGE_BASE_URL}/${poster_path}`
+}
+</script>
+
+<style scoped>
+
+</style>
