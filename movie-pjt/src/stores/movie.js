086b3c4b19fa4750324598c4b8e21a93039f6530
shsim <sh227sim@gmail.com>
2024-11-18 20:38:34 +0900

    FE-FEAT: create variable, fuctions for login logics

--- movie-pjt/src/stores/movie.js
+++ movie-pjt/src/stores/movie.js
@@ -2,7 +2,16 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useMovieStore = defineStore('movie', () => {
+  const token = ref(null)
   const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p'
+
+  const isLogin = computed(()=>{
+    if (token.value===null) {
+      return false
+    } else {
+      return true
+    }
+  })
 
   const movies = ref([
     { id: 1, title: 'Toy Story', tmdb_id: 862, summary:'summary of movie', release_date: '1995-04-01', genre:'Animation', poster_path:'/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg' },
@@ -18,5 +27,11 @@ export const useMovieStore = defineStore('movie', () => {
     const targetMovie = movies.value.find((movie)=>movie.id==Number(movieId))
     return targetMovie
   }
-  return { IMAGE_BASE_URL, movies, getImgUrl, getMovie, }
+
+  const logIn = function(payload) {
+    const username = payload.username
+    const password = payload.password
+    // axios 요청...! then -> token 값 받아오기
+  }
+  return { IMAGE_BASE_URL, movies, getImgUrl, getMovie, logIn, token, isLogin }
 })
