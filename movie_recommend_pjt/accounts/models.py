50cca2435babd2bfff024195a705b5af5bee0f19
PARK Jaeyoung <estel2005@naver.com>
2024-11-18 15:27:02 +0900

    BE_FEAT: modify User model fields (add birth, study_level, nickname, experience, achievement_level, followings)

--- movie_recommend_pjt/accounts/models.py
+++ movie_recommend_pjt/accounts/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    birth = models.DateField()
+    study_level = models.IntegerField(default='A1')
+    nickname = models.CharField(max_length=20, unique=True) # 닉네임 중복 방지
+    experience = models.IntegerField(default=0)
+    achievement_level = models.IntegerField(default=0)
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
