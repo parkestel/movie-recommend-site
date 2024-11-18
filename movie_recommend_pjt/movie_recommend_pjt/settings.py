aecc69c9985e3222bb1d6b8bda2b72d3d12cf897
PARK Jaeyoung <estel2005@naver.com>
2024-11-18 17:23:42 +0900

    BE_FEAT: registration rest_framework Authentication

--- movie_recommend_pjt/movie_recommend_pjt/settings.py
+++ movie_recommend_pjt/movie_recommend_pjt/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'movies',
     'accounts',
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+# SITE_ID = 1
+
+REST_FRAMEWORK = {
+#     # Authentication
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+#     # permission
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.AllowAny',
+#     ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
