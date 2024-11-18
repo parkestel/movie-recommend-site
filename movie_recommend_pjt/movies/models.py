8262d563c228a9405ba06a402b7c330fa38cefa4
PARK Jaeyoung <estel2005@naver.com>
2024-11-18 09:41:44 +0900

    BE_FEAT: create Star Model in movies.models.py

--- movie_recommend_pjt/movies/models.py
+++ movie_recommend_pjt/movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Star(models.Model):
+    tmdb_id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    profile_path = models.TextField()
+    characters = models.JSONField()
+    
