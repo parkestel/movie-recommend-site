a3e287df6cc2134304aae46c4ff32eb4a09346d3
PARK Jaeyoung <estel2005@naver.com>
2024-11-18 09:44:25 +0900

    BE_FEAT: create Genre Model in movies.models.py

--- movie_recommend_pjt/movies/models.py
+++ movie_recommend_pjt/movies/models.py
@@ -11,3 +11,7 @@ class Ott(models.Model):
     tmdb_id = models.IntegerField(primary_key=True)
     name = models.TextField()
     logo_path = models.TextField()
+
+class Genre(models.Model):
+    tmdb_id = models.IntegerField(primary_key=True)
+    name = models.TextField(max_length=20)
