25d9be4cd309780a2318d5c3c1d5964bbc9f4e86
shsim <sh227sim@gmail.com>
2024-11-19 17:21:04 +0900

    FE-FEAT: create, read in popup page for vocas

--- movie-pjt/src/components/VocaNotePopUpView/VocaCreate.vue
+++ movie-pjt/src/components/VocaNotePopUpView/VocaCreate.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-
+    <form>
+      <input type="text" id="word">
+      <input type="text" id="word_mean">
+      <input type="text" id="examples">
+      <textarea id="memo"></textarea>
+      <input type="submit" id="submit" value="update">
+    </form>
   </div>
 </template>
 
--- movie-pjt/src/components/VocaNotePopUpView/VocaListRead.vue
+++ movie-pjt/src/components/VocaNotePopUpView/VocaListRead.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-
+    <input type="checkbox" id="is_memorized">
+    <span>단어</span> : 
+    <span>단어 뜻</span><br>
+    <p>예문</p>
+    <p>메모</p>
   </div>
 </template>
 
