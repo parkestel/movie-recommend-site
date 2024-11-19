1ac20f3a7925579b682def499375f334afd68d55
PARK Jaeyoung <estel2005@naver.com>
2024-11-19 15:52:46 +0900

    BE_FEAT: change  study_level field int to text

--- movie_recommend_pjt/accounts/models.py
+++ movie_recommend_pjt/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     birth = models.DateField()
-    study_level = models.IntegerField(default='A1')
+    study_level = models.TextField(default='A1')
     nickname = models.CharField(max_length=20, unique=True) # 닉네임 중복 방지
     experience = models.IntegerField(default=0)
     achievement_level = models.IntegerField(default=0)
