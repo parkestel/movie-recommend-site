fda5ded17cf368af761dab4a284e85ac47bb28d6
shsim <sh227sim@gmail.com>
2024-11-19 20:43:26 +0900

    FE-FEAT:create profile components and follow section

---
+++ movie-pjt/src/components/ProfileView/ProfileFollow.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <span>Following : </span>|
+        <span>Follower : </span>
+        <br>
+        <button>(un)Follow</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- movie-pjt/src/views/ProfileView.vue
+++ movie-pjt/src/views/ProfileView.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-
+        <ProfileComponent/>
     </div>
 </template>
 
 <script setup>
-
+import ProfileComponent from '@/components/ProfileView/ProfileComponent.vue';
 </script>
 
 <style scoped>
