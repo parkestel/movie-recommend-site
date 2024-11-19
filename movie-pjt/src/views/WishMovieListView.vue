1a5677f22978a70b018b201ebcb7d3cde43a9821
shsim <sh227sim@gmail.com>
2024-11-19 11:15:28 +0900

    FE-FEAT:create wishlistview template and renderinig logic

--- movie-pjt/src/views/WishMovieListView.vue
+++ movie-pjt/src/views/WishMovieListView.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
         <h1>Movie WishList</h1>
+        <div v-if="wishMovies">
+            <WishMovieCard  
+            v-for="wishMovie in wishMovies"
+            :key="wishMovie.id"
+            :wish-movie="wishMovie"/>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { useMovieStore } from '@/stores/movie';
+import { onMounted, ref } from 'vue';
+import WishMovieCard from '@/components/WishMovieListView/WishMovieCard.vue';
 
-const wishMovies = ref()
+const store = useMovieStore()
+const wishMovies = ref(null)
+
+onMounted(()=>{
+    wishMovies.value = store.getWishMovies()
+})
 </script>
 
 <style scoped>
