a4ed7f446fefc1974ecfc5a9b97bd2edd2d0b698
PARK Jaeyoung <estel2005@naver.com>
2024-11-19 17:01:09 +0900

    BE_FEAT: create CustomRegisterSerializer for signup

--- movie_recommend_pjt/accounts/serializers.py
+++ movie_recommend_pjt/accounts/serializers.py
@@ -1,28 +1,39 @@
 from rest_framework import serializers
-from .models import User
+from rest_framework.validators import UniqueValidator
+from dj_rest_auth.registration.serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('nickname','username', 'first_name', 'last_name', 'email', 
-                  'birth', 'password','study_level')
-        extra_kwargs = {
-            'password': {'write_only': True},  # 비밀번호를 쓰기 전용으로 설정
+class CustomRegisterSerializer(RegisterSerializer):
+    birth = serializers.DateField()
+    study_level = serializers.CharField(max_length=10)
+    nickname = serializers.CharField(
+        max_length=20,
+        validators=[UniqueValidator(queryset=User.objects.all())]  # 고유성 검사 추가
+    )
+    first_name = serializers.CharField(max_length=30)  # 추가
+    last_name = serializers.CharField(max_length=30)   # 추가
+
+    def get_cleaned_data(self):
+        return {
+            'username': self.validated_data.get('username', ''),
+            'password': self.validated_data.get('password', ''),
+            'last_name': self.validated_data.get('last_name', ''),    # 추가
+            'first_name': self.validated_data.get('first_name', ''),  # 추가
+            'email': self.validated_data.get('email', ''),
+            'birth': self.validated_data.get('birth', ''),
+            'nickname': self.validated_data.get('nickname', ''),
+            'study_level': self.validated_data.get('study_level', ''),
         }
 
-        def create(self, validated_data):
-            user = User(
-                username=validated_data['username'],
-                first_name=validated_data.get('first_name'),
-                last_name=validated_data.get('last_name'),
-                email=validated_data['email'],
-                birth=validated_data.get('birth'),
-                nickname=validated_data['nickname'],
-                study_level=validated_data.get('study_level'),
-            )
-            user.set_password(validated_data['password'])
-            user.save()
-            return user
+    def save(self, request):
+        user = super().save(request)
+        user.last_name = self.validated_data.get('last_name')    # 추가
+        user.first_name = self.validated_data.get('first_name')  # 추가
+        user.birth = self.validated_data.get('birth')
+        user.nickname = self.validated_data.get('nickname')
+        user.study_level = self.validated_data.get('study_level')
+        user.save()
+        return user
+
