7be87496037b4baaf99112d80392a2c17df0c450
shsim <sh227sim@gmail.com>
2024-11-19 11:48:08 +0900

    FE-FEAT:create wishmoviecard component

---
+++ movie-pjt/src/components/WishMovieListView/WishMovieCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button v-if="!wishMovie.isLiked" @click="store.toggleLikeMovie(wishMovie.id)">🤍</button>
+    <button v-else  @click="store.toggleLikeMovie(wishMovie.id)">💖</button>
+    <div>
+      <img :src="store.getImgUrl(wishMovie.poster_path,200)" alt="">
+    </div>
+    <h5>{{ wishMovie.title }}</h5>
+    <button @click="moveToDetail(wishMovie.id)">Detail</button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  wishMovie:Object
+})
+
+import { useMovieStore } from "@/stores/movie"
+import { useRouter } from "vue-router"
+
+const store = useMovieStore()
+const router = useRouter()
+
+const moveToDetail = function (movieId) {
+  router.push({name:'movie-detail', params:{'movieid':movieId}})
+}
+</script>
+
+<style scoped>
+
+</style>
