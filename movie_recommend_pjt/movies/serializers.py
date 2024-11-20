da50c93a63f191d07329c3f897d902ce5daf6160
PARK Jaeyoung <estel2005@naver.com>
2024-11-20 13:52:02 +0900

    BE_FEAT: add MovieListSerailizers

--- movie_recommend_pjt/movies/serializers.py
+++ movie_recommend_pjt/movies/serializers.py
@@ -1,8 +1,54 @@
 from rest_framework import serializers
 
 from django.contrib.auth import get_user_model
-from .models import Movie
+from .models import Movie, Genre, Star, Ott, Director
 
 User = get_user_model()
 
+# 장르
+class GenreListSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
 
+# 배우
+class StarListSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Star
+        fields = '__all__'
+
+# ott
+class OttListSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Ott
+        fields = '__all__'
+
+# 감독
+class DirectorListSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = '__all__'
+
+
+# 한번에 넘길 무비 관련 모든 정보
+class MovieListSerializers(serializers.ModelSerializer):
+
+    genres = GenreListSerializers(many=True, read_only=True)
+    stars = StarListSerializers(many=True, read_only=True)
+    otts = OttListSerializers(many=True, read_only=True)
+    directors = DirectorListSerializers(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+# class MovieDetailSerializers(serializers.ModelSerializer):
+
+#     genres = GenreListSerializers(many=True, read_only=True)
+#     stars = StarListSerializers(many=True, read_only=True)
+#     otts = OttListSerializers(many=True, read_only=True)
+#     directors = DirectorListSerializers(many=True, read_only=True)
+
+#     class Meta:
+#         model = Movie
+#         fields = '__all__'
