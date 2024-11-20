c3f25bff830ed121e851368fd55228a50ebac419
PARK Jaeyoung <estel2005@naver.com>
2024-11-20 13:51:23 +0900

    BE_FEAT: add get_movies function to response all movies data

--- movie_recommend_pjt/movies/urls.py
+++ movie_recommend_pjt/movies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('movies/', views.get_movies),
+    path('movies/detail/', views.movie_detail),
 ]
--- movie_recommend_pjt/movies/views.py
+++ movie_recommend_pjt/movies/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Movie
+from .serializers import MovieListSerializers
 
-# Create your views here.
+
+@api_view(['GET'])
+# 영화 전체 조회
+def get_movies(request):
+    movies = Movie.objects.all()
+    serializer = MovieListSerializers(movies, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+# 한 영화 디테일 조회
+def movie_detail(request):
+    pass
+
+
+# @api_view(['GET'])
+# def filter_movie(request):
+#     genre_id = request.GET.get('genre')  # 쿼리 파라미터로 장르 ID 받음
+#     if not genre_id:
+#         return Response({"error": "Genre ID is required"}, status=400)
+
+#     try:
+#         # 장르 ID로 영화 필터링
+#         movies = Movie.objects.filter(genres__id=genre_id)
+#         serializer = MovieListSerializers(movies, many=True)
+#         return Response(serializer.data)
+#     except Genre.DoesNotExist:
+#         return Response({"error": "Invalid Genre ID"}, status=404)
