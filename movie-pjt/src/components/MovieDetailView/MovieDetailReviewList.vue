9e5a38af9d1e770fecb65e295f0e3a63d52a9666
shsim <sh227sim@gmail.com>
2024-11-20 11:01:29 +0900

    FE-FEAT: add review delete button, event, logic

--- movie-pjt/src/components/MovieDetailView/MovieDetailReviewList.vue
+++ movie-pjt/src/components/MovieDetailView/MovieDetailReviewList.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <MovieDetailReviewListItem v-for="review in reviews" :key="review.id" :review="review"/>
+    <MovieDetailReviewListItem
+    v-for="review in reviews" 
+    :key="review.id" 
+    :review="review"
+    @delete-event="deleteReview"/>
   </div>
 </template>
 
@@ -13,6 +17,12 @@ const reviews = ref([
   {id:2, content:'내용이 재미있고, 좋아요!', liked_user:18, nickname:'user2'},
   {id:3, content:'저는 영화 반복하면서 계속 들었더니 귀가 트였어용', liked_user:11, nickname:'user3'},
 ])
+
+const deleteReview = function (id) {
+  console.log(id)
+  const targetid = reviews.value.findIndex((review)=>review.id === id)
+  reviews.value.splice(targetid, 1)
+}
 </script>
 
 <style scoped>
