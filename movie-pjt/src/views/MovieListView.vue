6adb4aabdfc04dd4c15edea66dfca0866655c385
shsim <sh227sim@gmail.com>
2024-11-20 13:33:31 +0900

    FE-FEAT: add search logic

--- movie-pjt/src/views/MovieListView.vue
+++ movie-pjt/src/views/MovieListView.vue
@@ -1,12 +1,20 @@
 <template>
 <div>
     <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <input 
+        class="form-control me-2" 
+        type="search" 
+        placeholder="movie title, actor, director, release year..." 
+        aria-label="Search"
+        @input="searchGroup($event)">
     </form>
     <!-- filter -->
     <div>
-        <MovieCard v-for="movie in store.movies" :key="movie" :movie="movie"/>
+        <MovieCard 
+        v-for="movie in searchedMovies" 
+        :key="movie" 
+        :movie="movie"
+        />
     </div>
 </div>
 </template>
@@ -14,10 +22,23 @@
 <script setup>
 import MovieCard from "@/components/MovieListView/MovieCard.vue"
 import { useMovieStore } from "@/stores/movie"
+import { onMounted, ref } from "vue";
 
 const store = useMovieStore()
+const searchedMovies = ref([])
+
+const searchGroup = function (event) {
+    const query = event.target.value.toLowerCase(); // 대소문자 구분 없이 검색
+    searchedMovies.value = store.movies.filter(movie => 
+        movie.title.toLowerCase().includes(query) || 
+        movie.release_date.includes(query) 
+    )
+}
+
+onMounted(()=>{
+    searchedMovies.value = store.movies
+})
 </script>
 
 <style scoped>
-
 </style>
