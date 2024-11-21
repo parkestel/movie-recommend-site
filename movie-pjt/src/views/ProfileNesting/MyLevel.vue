e9e753a4977866019b004ea66c43961df24bd70a
shsim <sh227sim@gmail.com>
2024-11-21 23:20:26 +0900

    FE-FIX: fix bug of profile page in user level

--- movie-pjt/src/views/ProfileNesting/MyLevel.vue
+++ movie-pjt/src/views/ProfileNesting/MyLevel.vue
@@ -1,21 +1,18 @@
 <template>
-    <div v-if="userInfo">
+    <div>
         <h4>My Study Level</h4>
-        <p>ex point : {{ userInfo.experience }}</p>
-        <p>level : {{ userInfo.achievement_level }}</p>
-        <p>Now studying : {{ userInfo.study_level }}</p>
+        <p>ex point : {{ userProfile.experience }}</p>
+        <p>level : {{ userProfile.achievement_level }}</p>
+        <p>Now studying : {{ userProfile.study_level }}</p>
     </div>
 </template>
 
 <script setup>
 import { useMovieStore } from '@/stores/movie';
 import { onMounted, ref } from 'vue';
+import { storeToRefs } from 'pinia';
 const store = useMovieStore()
-const userInfo = ref(null)
-
-onMounted(()=>{
-    userInfo.value = store.userProfile
-})
+const { userProfile } = storeToRefs(store)
 </script>
 
 <style scoped>
