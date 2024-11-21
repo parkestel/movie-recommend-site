4d1fdb376b96ff463b13cf5ea44712e12c28b3bc
PARK Jaeyoung <estel2005@naver.com>
2024-11-21 10:08:43 +0900

    BE_FEAT: update create_voca_note function

--- movie_recommend_pjt/voca_notes/views.py
+++ movie_recommend_pjt/voca_notes/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .serializers import VocaNoteListSerializers
+from .serializers import VocaNoteSerializers,VocaSerializers
 from .models import VocaNote, Voca
 from movies.models import Movie
 
@@ -13,20 +14,53 @@ User = get_user_model()
 # create(내 단어장 로그인한 사용자랑 pk확인), read(다른 사람들 단어장 읽는), update(내 단어장 로그인한 사용자랑 pk확인), delete(내 단어장 로그인한 사용자랑 pk확인)
 
 # Vocanote 생성 : 보는 사람이랑 로그인한 사용자랑 같아야 단어장 생성 가능.
-@api_view('POST', 'DELETE') 
+@api_view('GET','POST', 'DELETE') 
+@permission_classes([IsAuthenticated])  # 로그인된 사용자만 접근 가능
 def create_voca_note(request, movie_pk, user_pk):
-    movie = Movie.objects.get(pk=movie_pk)
-    person = User.objects.get(pk=user_pk)
+
+    person = get_object_or_404(User, pk=user_pk)
+    movie = get_object_or_404(Movie, pk=movie_pk)
     me = request.user
 
-    if me == person:
-        serializer = VocaNoteListSerializers(근데,,,,그거 어떻게 해 )
-        # 나니까 단어장 자동 생성
-        # vocanote 하나 만들어질 때, 
-        # if 근데 이미 이 영화에 단어장있으면 못만들어 디버깅 메세지 
-        pass
-    else:
-        return Response('다른 사람 단어장은 생성할 수 없습니다.')
+    if request.method == 'GET':
+
+        voca_notes = VocaNote.objects.filter(users=person, movies=movie)
+        if not voca_notes.exists():
+            return Response({'error': '해당 영화에 대한 단어장이 없습니다.'}, status=404)
+        
+        if person != me and not any(note.is_public for note in voca_notes):
+            return Response({'error': '해당 단어장은 비공개 상태입니다.'}, status=403)
+        
+        serializer = VocaNoteSerializers(voca_notes, many=True)
+        return Response(serializer.data, status=200)
+
+    elif request.method == 'POST':
+
+        if me != person:
+            return Response({'error': '자신의 단어장만 생성할 수 있습니다.'}, status=403)
+        
+
+        existing_note = VocaNote.objects.filter(users=me, movies=movie).first()
+        if existing_note:
+            return Response({'error': '이미 해당 영화에 대한 단어장이 존재합니다.'}, status=400)
+
+        voca_note = VocaNote.objects.create(is_public=True)  # 기본 공개 상태로 생성
+        voca_note.users.add(me)
+        voca_note.movies.add(movie)
+        voca_note.save()
+
+        serializer = VocaNoteSerializers(voca_note)
+        return Response(serializer.data, status=201)
+
+    elif request.method == 'DELETE':
+        # 삭제 - 로그인한 사용자만 가능
+        voca_note = VocaNote.objects.filter(users=me, movies=movie).first()
+        if not voca_note:
+            return Response({'error': '삭제할 단어장이 존재하지 않습니다.'}, status=404)
+        
+        voca_note.delete()
+        return Response({'message': '단어장이 삭제되었습니다.'}, status=200)
+
 
 
 # 단어장 보여주기만
