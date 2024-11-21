891a22402cb2507dc958ea60a5b37943db3d791c
PARK Jaeyoung <estel2005@naver.com>
2024-11-21 16:59:40 +0900

    BE_FEAT: modify url voca_notes/ to voca

--- movie_recommend_pjt/movie_recommend_pjt/urls.py
+++ movie_recommend_pjt/movie_recommend_pjt/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
-    path('voca-notes/', include('voca_notes.urls')),
+    path('voca/', include('voca_notes.urls')),
 ]
