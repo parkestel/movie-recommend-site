778397e3adb58ccab4c2b8a10596d7e6c8a935f1
PARK Jaeyoung <estel2005@naver.com>
2024-11-23 02:16:14 +0900

    BE_FEAT: add UserSerializer

--- movie_recommend_pjt/movies/serializers.py
+++ movie_recommend_pjt/movies/serializers.py
@@ -5,6 +5,11 @@ from .models import Movie, Genre, Star, Ott, Director, Comment
 
 User = get_user_model()
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
 # 장르
 class GenreListSerializers(serializers.ModelSerializer):
     class Meta:
