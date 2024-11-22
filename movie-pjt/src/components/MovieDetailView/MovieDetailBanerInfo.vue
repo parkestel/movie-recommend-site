837f801409fa59a261e2f42109048639d4eaecb8
shsim <sh227sim@gmail.com>
2024-11-23 00:49:16 +0900

    FE-FIX: fix movie genres in moviedetail page, banerinfo component

--- movie-pjt/src/components/MovieDetailView/MovieDetailBanerInfo.vue
+++ movie-pjt/src/components/MovieDetailView/MovieDetailBanerInfo.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>제목: {{ movieInfo.title }}</h3>
     <p>summary: {{ movieInfo.summary }}</p>
-    <p>genre: {{ movieInfo.genre }}</p>
+    <p>genre: {{ movieInfo.genres.map(genre => genre.name).join(' ') }}</p>
     <p>release_date: {{ movieInfo.release_date }}</p>
     <button v-if="!store.isLikedMovie(movieInfo.id)" @click="store.addToggleWishMovie(movieInfo.id)">🤍</button>
     <button v-else  @click="store.addToggleWishMovie(movieInfo.id)">💖</button>
