d9f833823157008e4ac38484404ce361df000667
shsim <sh227sim@gmail.com>
2024-11-22 15:53:29 +0900

    FE-FIX: add korean title in movie card

--- movie-pjt/src/components/MovieListView/MovieCard.vue
+++ movie-pjt/src/components/MovieListView/MovieCard.vue
@@ -6,6 +6,7 @@
     <div>
       <img :src="store.getImgUrl(movie.poster_path,200)" alt="">
     </div>
+    <h4>{{ movie.title_kr }}</h4>
     <h5>{{ movie.title }}</h5>
     <button @click="moveToDetail(movie.id)">Detail</button>
   </div>
