e1ac331e968f7537c5e19be369300d59f9fdb438
parkestel <park960614@gmail.com>
2024-11-23 12:59:09 +0900

    BE_FEAT: add user.experience+=100 logic when login

--- movie_recommend_pjt/accounts/urls.py
+++ movie_recommend_pjt/accounts/urls.py
@@ -1,26 +1,28 @@
 from django.urls import path, include
 from . import views
+from .views import CustomLoginView
 
 urlpatterns = [
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('delete/', views.delete_user),
-    path('<int:user_pk>/follow/', views.followings),
-    path('dj-rest-auth/user/', views.login_user_data),
-    path('dj-rest-auth/user/<str:username>/', views.profile),
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("dj-rest-auth/login/", CustomLoginView.as_view()),
+    path("delete/", views.delete_user),
+    path("<int:user_pk>/follow/", views.followings),
+    path("dj-rest-auth/user/", views.login_user_data),
+    path("dj-rest-auth/user/<str:username>/", views.profile),
 ]
 
 # 회원가입
-    # accounts/ dj-rest-auth/registration/
+# accounts/ dj-rest-auth/registration/
 # 로그인
-    # accounts/ dj-rest-auth/ login/ [name='rest_login']
+# accounts/ dj-rest-auth/ login/ [name='rest_login']
 # 로그아웃
-    # accounts/ dj-rest-auth/ logout/ [name='rest_logout']
+# accounts/ dj-rest-auth/ logout/ [name='rest_logout']
 # 유저 정보 반환
-    # accounts/ dj-rest-auth/ user/ [name='rest_user_details']
+# accounts/ dj-rest-auth/ user/ [name='rest_user_details']
 # 패스워드 초기화(이메일로 전송)
-    # accounts/ dj-rest-auth/ password/reset/ [name='rest_password_reset']
+# accounts/ dj-rest-auth/ password/reset/ [name='rest_password_reset']
 # 패스워드 초기화 (이메일 확인 후 초기화 페이지)
-    # accounts/ dj-rest-auth/ password/reset/confirm/ [name='rest_password_reset_confirm']
+# accounts/ dj-rest-auth/ password/reset/confirm/ [name='rest_password_reset_confirm']
 # 패스워드 변경
-    # accounts/ dj-rest-auth/ password/change/ [name='rest_password_change']
+# accounts/ dj-rest-auth/ password/change/ [name='rest_password_change']
--- movie_recommend_pjt/accounts/views.py
+++ movie_recommend_pjt/accounts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import logout
+from dj_rest_auth.views import LoginView
 
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
-from rest_framework.decorators import authentication_classes, api_view, permission_classes
+from rest_framework.decorators import (
+    authentication_classes,
+    api_view,
+    permission_classes,
+)
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -13,15 +18,32 @@ from .serializers import PersonUserDetailsSerializer, CustomUserDetailsSerialize
 
 User = get_user_model()
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def login_user_data(request):
     user = request.user
     serializer = CustomUserDetailsSerializer(user)
     return Response(serializer.data)
 
 
+class CustomLoginView(LoginView):
+    def post(self, request, *args, **kwargs):
+        # 로그인 요청 처리
+        response = super().post(request, *args, **kwargs)
 
-@api_view(['GET'])
+        # 현재 로그인된 사용자 정보 가져오기
+        user = self.request.user
+
+        if user.is_authenticated:  # 사용자가 인증된 경우에만
+            # experience 필드 업데이트
+            user.experience += 100
+            user.save()
+
+        return response
+
+
+@api_view()
+@api_view(["GET"])
 @authentication_classes([TokenAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 #  특정 사용자의 프로필 정보 조회 API (로그인 인증된 사용자만 접근 가능)
@@ -29,35 +51,33 @@ def profile(request, username):
     person = get_object_or_404(User, username=username)
     serializer = PersonUserDetailsSerializer(person)
     return Response(serializer.data)
- 
 
 
 # 회원 탈퇴 기능 & 로그아웃까지 연동
-@api_view(['POST'])
+@api_view(["POST"])
 @authentication_classes([TokenAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def delete_user(request):
-    message = ''
+    message = ""
     if request.user.is_authenticated:
         username = request.user.username
         request.user.delete()
         message = {
             "message": f"회원탈퇴가 완료되었습니다. 삭제된 사용자: {username}",
-            "status": "success"
+            "status": "success",
         }
         return Response(data=message, status=status.HTTP_204_NO_CONTENT)
     else:
         # 인증되지 않은 경우
         message = {
             "message": "회원탈퇴에 실패했습니다. 사용자가 인증되지 않았습니다.",
-            "status": "failure"
+            "status": "failure",
         }
         return Response(data=message, status=status.HTTP_401_UNAUTHORIZED)
 
 
-
 # 팔로잉 기능
-@api_view(['POST'])
+@api_view(["POST"])
 def followings(request, user_pk):
     person = User.objects.get(pk=user_pk)
     me = request.user
@@ -65,11 +85,19 @@ def followings(request, user_pk):
         if me in person.followers.all():
             # 팔로잉 취소
             person.followers.remove(me)
-            return Response({'status': 'following 취소', '끊은 person': person.nickname}, status=status.HTTP_200_OK)
+            return Response(
+                {"status": "following 취소", "끊은 person": person.nickname},
+                status=status.HTTP_200_OK,
+            )
         else:
             # 팔로잉 추가
             person.followers.add(me)
-            return Response({'status': 'following', 'followings': person.nickname}, status=status.HTTP_200_OK)
+            return Response(
+                {"status": "following", "followings": person.nickname},
+                status=status.HTTP_200_OK,
+            )
     else:
-            return Response({'error': '자기 자신을 팔로우할 수 없습니다.'}, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response(
+            {"error": "자기 자신을 팔로우할 수 없습니다."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
