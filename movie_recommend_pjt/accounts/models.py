b11b5b3b8acf943eef81dafeb622f18c4e8a98b5
PARK Jaeyoung <estel2005@naver.com>
2024-11-25 10:45:01 +0900

    BE_FIX: modify default value in achivement_level default=1

--- movie_recommend_pjt/accounts/models.py
+++ movie_recommend_pjt/accounts/models.py
@@ -7,5 +7,5 @@ class User(AbstractUser):
     study_level = models.CharField(max_length=5, default='A1')
     nickname = models.CharField(max_length=20, unique=True) # 닉네임 중복 방지
     experience = models.IntegerField(default=0)
-    achievement_level = models.IntegerField(default=0)
+    achievement_level = models.IntegerField(default=1)
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
