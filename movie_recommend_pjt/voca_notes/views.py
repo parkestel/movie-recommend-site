2a3195a02b244326fb3335db205fe9a7a242eac0
PARK Jaeyoung <estel2005@naver.com>
2024-11-25 10:05:52 +0900

    BE_FEAT: add new makemigrations & migrate bc VocaNoteHistory

--- movie_recommend_pjt/voca_notes/views.py
+++ movie_recommend_pjt/voca_notes/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-from .serializers import VocaNoteSerializers, VocaNoteAllSerializers, VocaSerializers, 
+from .serializers import VocaNoteSerializers, VocaNoteAllSerializers, VocaSerializers
 from .models import VocaNote, Voca, VocaNoteHistory
 from movies.models import Movie
 
