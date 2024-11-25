03af6538ba3a36abe2382de16af6cedcf1cd9c56
PARK Jaeyoung <estel2005@naver.com>
2024-11-25 09:59:57 +0900

    BE_FEAT: create VocaNoteHistory model for += user.experience logic

--- movie_recommend_pjt/voca_notes/models.py
+++ movie_recommend_pjt/voca_notes/models.py
@@ -18,3 +18,13 @@ class VocaNote(models.Model):
     movies = models.ManyToManyField(Movie, related_name="voca_notes")
     users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="voca_notes")
     vocas = models.ManyToManyField(Voca, related_name="voca_notes")
+
+# 영화별 단어장 생성 기록을 관리하는 모델
+class VocaNoteHistory(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('movie', 'user')  # 같은 영화에 대해 한 명의 사용자만 단어장을 생성할 수 있도록 유니크 제약
+
